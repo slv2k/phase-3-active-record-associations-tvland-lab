94fb81e78c1e3577f887065e50b932b1118a5c13
samuel <samuelmmanor@gmail.com>
2022-03-01 19:34:06 -0500

    commit tree 69095d1034c986a0cccf717169cf99afb2198428

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+
 class Show < ActiveRecord::Base
+    # returns list of all character instances associated with show
+    has_many :characters
+    has_many :actors, through: :characters
+    # returns network instance this show belongs to
+    belongs_to :network
   
-end
+    # returns an array of the full names of each actor associated with a show
+    def actors_list
+      self.actors.map do |actor|
+        actor.full_name
+      end
+    end
+  end
